8cf9b8a5d2ce41b5fd912835719493191d90e747
Sudharsan <sudharsan.pattan@chronus.com>
2023-07-11 00:33:43 +0530

    Rubygems 3.4 compatibility

--- lib/cbc-wrapper.rb
+++ lib/cbc-wrapper.rb
@@ -1,5 +1,5 @@
 module CbcWrapper
 end
 
-require File.expand_path("../../ext/cbc-wrapper/cbc_wrapper", __FILE__)
+require File.expand_path("../cbc_wrapper", __FILE__)
 require File.expand_path("../cbc-wrapper/version", __FILE__)
